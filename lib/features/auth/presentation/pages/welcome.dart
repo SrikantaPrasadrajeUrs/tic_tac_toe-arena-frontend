6b933724f00fa1b9fa75ab94b44b583733d0f4e3
SrikantaPrasadrajeUrs <srikantaprasadrajeurs456@gmail.com>
2026-01-03 09:53:36 +0530

    class code

--- lib/features/auth/presentation/pages/welcome.dart
+++ lib/features/auth/presentation/pages/welcome.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Welcome extends StatelessWidget {
@@ -7,8 +8,7 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-    final imageHeight = height * 0.6;
-
+    final imageHeight = height * .6;
     return Scaffold(
       backgroundColor: Colors.black,
       body: Stack(
@@ -17,79 +17,55 @@ class Welcome extends StatelessWidget {
             top: 0,
             left: 0,
             right: 0,
-            bottom: imageHeight/2,
+            bottom: imageHeight / 2,
             child: Image.asset(
               "assets/images/peacock_feature.jpeg",
               fit: BoxFit.cover,
             ),
           ),
-
           Positioned(
-            top: imageHeight-400,
             left: 0,
             right: 0,
             bottom: 0,
+            top: imageHeight - 400,
             child: ClipRect(
               child: ShaderMask(
                 shaderCallback: (rect) {
-                  return const LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
+                  return LinearGradient(
+                    begin: AlignmentGeometry.topCenter,
+                    end: AlignmentGeometry.bottomCenter,
+                    stops: [0.0, .35, 0.6, 0.1],
                     colors: [
                       Colors.transparent,
                       Colors.black54,
                       Colors.black,
-                      Colors.black,
+                      Colors.black
                     ],
-                    stops: [0.0, 0.35, 0.6, 1.0],
                   ).createShader(rect);
                 },
                 blendMode: BlendMode.dstIn,
                 child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-                  child: Container(
-                    color: Colors.black,
-                  ),
+                  filter: ImageFilter.blur(sigmaX: 30, sigmaY: 40),
+                  child: Container(color: Colors.black),
                 ),
               ),
             ),
           ),
-
           Align(
             alignment: Alignment.bottomCenter,
             child: SafeArea(
               top: false,
               child: Padding(
-                padding: const EdgeInsets.fromLTRB(24, 0, 24, 32),
+                padding: const EdgeInsets.fromLTRB(24, 0, 24, 30),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Text(
-                      "TIC TAC TOE",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w700,
-                        letterSpacing: 1.4,
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    Text(
-                      "Challenge your mind. Compete in real time.\nClassic strategy, modern experience.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 15,
-                        height: 1.4,
-                      ),
-                    ),
+                  children: [
+                    Text("TIC TAC TOE", style: TextStyle(color: Colors.white)),
                   ],
                 ),
               ),
             ),
           ),
-
         ],
       ),
     );
